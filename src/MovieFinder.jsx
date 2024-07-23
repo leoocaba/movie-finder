8429c5aa81f739e818f8df509f10f67abb93e385
leoocaba <leoo.caballero@gmail.com>
2024-07-23 18:55:43 -0300

    Se editó la adaptación responsive y scrollbar

--- src/MovieFinder.jsx
+++ src/MovieFinder.jsx
@@ -32,18 +32,18 @@ export const MovieFinder = () => {
     <>
       <header className="__movie-finder-container container p-0 m-0">
         <div className="row p-0 m-0">
-          <div className="__movie-finder container p-0 m-0">
-            <h1 className="col text-center text-white mb-1 mt-3">
+          <div className="__movie-finder container d-md-flex flex-md-row p-0 m-0">
+            <h1 className="col text-center text-md-end text-white mb-1 mt-3 my-md-auto mx-md-1 p-md-0">
               Movie Finder
             </h1>
             <form
-              className="d-flex justify-content-center mx-0 px-0 mb-3 mt-1"
+              className="col d-flex justify-content-center justify-content-md-start mx-0 px-0 mb-3 mt-1 my-md-1 mx-md-1 py-md-1"
               onSubmit={handleSubmit}
             >
               <div className="d-flex">
                 <input
                   type="text"
-                  className="form-control rounded-2 rounded-end-0"
+                  className="form-control rounded-2 rounded-end-0 py-1"
                   placeholder="Toy Story..."
                   value={search}
                   onChange={handleInputChange}
@@ -52,7 +52,7 @@ export const MovieFinder = () => {
               <div className="d-flex">
                 <button
                   type="submit"
-                  className="btn btn-inline-block rounded-2 rounded-start-0"
+                  className="btn btn-inline-block rounded-2 rounded-start-0 py-1"
                 >
                   Buscar
                 </button>
